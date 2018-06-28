bb4833ab9d3a1ec1549b5a82c5e4785251c69d73
Bruna Queiroz <bsqueiroz98@gmail.com>
2018-06-28 15:22:46 -0300

    new backtracking

--- R/gradientproj.R
+++ R/gradientproj.R
@@ -1,41 +1,27 @@
 #Reference: Kelley - Iterative Methods for optimization
-gradientproj <- function(obj.list, x.list, constraint, maxNI = 50, eps = 1e-4, methodgradient = c("ffd", "cfd")) {
-  #Copy of values
-  obj <- obj.list$f
-
-  projection <- function(constraint, obj) {
-    out <- pmax(x,constraint$l)
-    out <- pmin(out, constraint$u)
-
-
-    return(obj(out))
-  }
-
-  if(!("df") %in% names(obj.list)) {
-    dFun <- gradient
-  } else {
-    dFun <- obj.list$df
-  }
-  if(!("dfx") %in% names(x.list)) {
-    dfx.k1 <- dFun(obj, x.list$x, method = methodgradient)
-  } else {
-    dfx.k1 <- x.list$dfx
-  }
+gradientproj <- function(obj.list, x.list, constraint, maxNI = 50, eps = 1e-4, alpha0 = 1, c = 1e-4, rho = 0.5, ...) {
+  ## Initial considerations
+  # Check input
+  out <- check_parameters(obj.list, x.list, ...)
+  obj.list <- out[[1]]
+  x.list <- out[[2]]
 
   x.k1 <- x.list
+  dfx.k1 <- x.list$dfx
 
   for(i in 1:maxNI) {
     dfx.k <- dfx.k1
     x.k <- x.k1
 
-    searchD <- dfx.k
+     searchD <- projection(x.k$x - dfx.k, constraint) - x.k$x
+    # x.proj <- projection(x.k$x, constraint)
+    # x.listproj <- evaluate(obj.list, x.proj)
+    # searchD <- -x.listproj$dfx
 
     #Backtracking
-    alpha <- backtracking.gp(projection, x.k, searchD, alpha0, rho, c)
-    xp <- x.k$x + as.numeric(searchD*alpha)
-    x.k1 <- list(x = xp,
-                 fx = obj(xp),
-                 dfx = dFun(obj, xp, method = methodgradient))
+    alpha <- backtracking.gp(obj.list, x.k, constraint, searchD, alpha0, rho, c)
+    x.k1$x <- x.k$x + alpha*searchD
+    x.k1 <- evaluate(obj.list, x.k1$x)
 
     #Gradient in the new point
     dfx.k1 <- x.k1$dfx
@@ -45,11 +31,20 @@ gradientproj <- function(obj.list, x.list, constraint, maxNI = 50, eps = 1e-4, m
       break
     }
   }
-
+  return(x.k1)
 
 }
 
-backtracking.gp <- function(f, x.list, searchD, alpha0 = 1, rho = 0.5, c = 1e-4)
+projection <- function(x, constraint) {
+  u <- constraint$xmax
+  l <- constraint$xmin
+  out1 <- pmin(x,u)
+  out <- pmax(out1, l)
+
+  return(out)
+}
+
+backtracking.gp <- function(obj.list, x.list, constraint, searchD, alpha0 = 1, rho = 0.5, c = 1e-4)
 {
   x <- x.list$x
   fx <- x.list$fx
@@ -57,18 +52,14 @@ backtracking.gp <- function(f, x.list, searchD, alpha0 = 1, rho = 0.5, c = 1e-4)
 
   alpha <- alpha0 #initial alpha
   sdfx <- sum(dfx * searchD) #dot product between dfx and searchD
+  x.proj <- projection(x - dfx, constraint)
 
-  while( f(x + alpha*searchD) > fx + c*alpha*sdfx )
+  while( obj.list$f(x) - obj.list$f(x.proj) > fx + c*alpha*sdfx )
   {
     alpha <- alpha*rho
+    x.proj <- projection(x - dfx, constraint)
   }
 
   return(alpha)
 }
 
-
-projection <- function(x, l, u) {
-  out <- pmax(x,l)
-  out <- pmin(out, u)
-  return(out)
-}
